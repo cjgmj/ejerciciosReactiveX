1f019199dc46d1dd283c17aef5b14452e7b2371b
cjgmj <cjgmj@hotmail.com>
2020-06-25 14:52:01 +0200

    Primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { Observable } from 'rxjs';
 
+// Es un estándar añadir el $ como sufijo del nombre de la variable para indicar que es un observable
+// const obs$ = Observable.create();
+const obs$ = new Observable<string>( subs => {
+    subs.next('Hola');
+    subs.next('Mundo');
 
-console.log('Hola Mundo!');
+    // Lo que emita tras el complete no se notificará a los suscriptores
+    subs.complete();
 
+    subs.next('Hola');
+    subs.next('Mundo');
+});
 
-
-
-
-
-
+// Para que un observable se ejecute necesita al menos una suscripción
+obs$.subscribe(console.log);
