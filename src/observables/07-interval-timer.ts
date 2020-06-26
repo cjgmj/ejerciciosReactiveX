b2cca73c9ceaba55dc08415242c66caf57675855
cjgmj <cjgmj@hotmail.com>
2020-06-26 16:18:31 +0200

    Internal y timer

---
+++ src/observables/07-interval-timer.ts
@@ -0,0 +1,24 @@
+import { interval, timer } from 'rxjs';
+
+const observer= {
+    next: val => console.log('next:', val),
+    complete: () => console.log('complete')
+}
+
+// Son observables asíncronos
+// const interval$ = interval(1000);
+
+// const timer$ = timer(2000);
+
+// Funciona como un interval una vez transcurrido el tiempo de espera pasado por el primer parámetro
+// const timer$ = timer(2000, 1000);
+
+// Emite el valor en el momento indicado
+const hoyEn5 = new Date();
+hoyEn5.setSeconds(hoyEn5.getSeconds() + 5);
+const timer$ = timer(hoyEn5);
+
+console.log('Inicio');
+// interval$.subscribe(observer);
+timer$.subscribe(observer);
+console.log('Fin');
