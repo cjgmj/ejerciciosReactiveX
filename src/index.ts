fdea76b01070b49eae246fb86d7e0b4c43b8ae1d
cjgmj <cjgmj@hotmail.com>
2020-06-27 11:29:22 +0200

    pluck

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { range, fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, pluck } from 'rxjs/operators';
 
 // range(1, 5).pipe(
 //     // El primer tipo es lo que recibe y el segundo lo que devuelve
@@ -13,4 +13,11 @@ const keyupCode$ = keyup$.pipe(
     map( event => event.code)
 );
 
+const keyupPlick$ = keyup$.pipe(
+    // pluck('key')
+    pluck('target', 'baseURI')
+);
+
+keyup$.subscribe(console.log);
 keyupCode$.subscribe(code => console.log('map', code));
+keyupPlick$.subscribe(code => console.log('pluck', code));
